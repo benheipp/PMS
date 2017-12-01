e2b724fec14ea1bf80682debd123022a7e84e5fb
Adam <adam.kagy@gmail.com>
2017-12-01 10:01:15 -0700

    PMS-75 - Add doc key link reference for product search

--- src/Product/product-doc-key-row.js
+++ src/Product/product-doc-key-row.js
@@ -4,12 +4,12 @@ var ProductDocKeyRow = React.createClass({
 	    		return (
   <div className='row'>
     <div className='col-sm-8'>
-	    				Doc Key:{this.props.dockey.docKey}
+	    				Doc Key:<a target="_window" href={`catalog?doc_key=${this.props.dockey.docKey}&store_id=${this.props.dockey.store_id}&store_name=${this.props.dockey.store_name}`}>{this.props.dockey.docKey}</a>
     </div>
         <div className='col-sm-1'>
         </div>
     <div className='col-sm-3'>
-             Store:{this.props.dockey.store}
+             Store:{this.props.dockey.store_name}
     </div>
   </div>)
 	    }
