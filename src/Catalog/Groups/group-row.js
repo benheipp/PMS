7061b4605d40dbf7e14bf680b889f84387094b18
benheipp <Ben.H@outdoornetwork.com>
2018-01-02 17:31:37 -0500

    PMS-81: updates grouping for site level and adds is active

--- src/Catalog/Groups/group-row.js
+++ src/Catalog/Groups/group-row.js
@@ -5,17 +5,23 @@ const GroupRow = React.createClass({
     return {
       name: this.props.name,
       sortOrder: this.props.sortOrder === null ? '' : this.props.sortOrder,
+      isActive: this.props.isActive,
     };
   },
   handleNameChange: function (event) {
     const value = event.target.value;
     this.setState({name: value});
-    this.props.setGroup(this.props.id, value, this.state.sortOrder);
+    this.props.setGroup(this.props.id, value, this.state.sortOrder, this.state.isActive);
   },
   handleSortOrderChange: function (event) {
     const value = event.target.value;
     this.setState({sortOrder: value});
-    this.props.setGroup(this.props.id, this.state.name, value);
+    this.props.setGroup(this.props.id, this.state.name, value, this.state.isActive);
+  },
+  handleIsActiveChange: function (event) {
+    const value = event.target.checked;
+    this.setState({isActive: value});
+    this.props.setGroup(this.props.id, this.state.name, this.state.sortOrder, value);
   },
   render: function() {
     return (
@@ -38,6 +44,14 @@ const GroupRow = React.createClass({
             onChange={this.handleSortOrderChange}
           />
         </td>
+        <td>
+          <input
+            type="checkbox"
+            className="form-control input-sm"
+            checked={this.state.isActive}
+            onChange={this.handleIsActiveChange}
+          />
+        </td>
       </tr>
     );
   }
@@ -46,6 +60,7 @@ const GroupRow = React.createClass({
 GroupRow.propTypes ={
   name: React.PropTypes.string,
   sortOrder: React.PropTypes.number,
+  isActive: React.PropTypes.bool,
 };
 
 export default GroupRow;
